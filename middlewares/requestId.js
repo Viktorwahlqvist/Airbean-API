23acef9c20606aacca4e65b53da843ec63f3257d
tessaan <thereseulvesand@hotmail.com>
2025-04-03 09:24:19 +0200

    Beskrivning av ändringar

--- middlewares/requestId.js
+++ middlewares/requestId.js
@@ -1,10 +1,10 @@
-const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from "uuid";
 
 const requestId = (req, res, next) => {
   const id = uuidv4();
   req.requestId = id;
-  res.setHeader('X-Request-Id', id);
+  res.setHeader("X-Request-Id", id);
   next();
 };
 
-module.exports = requestId;
+export default requestId;
