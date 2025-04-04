9a5ca113c7a3a764d3c4b1e9be5c3b4a35f14073
tessaan <thereseulvesand@hotmail.com>
2025-04-04 09:31:54 +0200

    Lade till idValidation middleware för ID-koll

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,21 @@
 import express from "express";
 const router = express.Router();
 
-// Exempelroute
+// Importera middleware
+import { idValidation } from "../middlewares/validation.js";
+
+// (Exempel) importera din controller
+import { getUserById, deleteUser } from "../controllers/userController.js";
+
+// En route som hämtar en användare med ID
+router.get("/users/:id", idValidation, getUserById);
+
+// En route som tar bort en användare med ID
+router.delete("/users/:id", idValidation, deleteUser);
+
+// Testroute
 router.get("/", (req, res) => {
-  res.send("Assortment works!");
+  res.send("User-routes funkar!");
 });
 
 export default router;
